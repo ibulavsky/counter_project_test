43ae983ae692bf80da725a7105391365b737445d
ibulavsky <ibulavsky@gmail.com>
2019-11-11 22:19:57 +0300

    Updates

---
+++ precache-manifest.9a01d42b27eff147b4efc17ec70f9846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1617750c44732d4abd74345aee845fa2",
+    "url": "/counter_project_test/index.html"
+  },
+  {
+    "revision": "a74f137c8c0fd242dc7c",
+    "url": "/counter_project_test/static/css/main.608752f3.chunk.css"
+  },
+  {
+    "revision": "dfd94915895434677aa5",
+    "url": "/counter_project_test/static/js/2.1914613d.chunk.js"
+  },
+  {
+    "revision": "a74f137c8c0fd242dc7c",
+    "url": "/counter_project_test/static/js/main.aed2669c.chunk.js"
+  },
+  {
+    "revision": "f2e508fb8a482390dadf",
+    "url": "/counter_project_test/static/js/runtime-main.ea5b1463.js"
+  },
+  {
+    "revision": "a188b9841478dac76315b5b296be2b69",
+    "url": "/counter_project_test/static/media/SettingsUI.a188b984.png"
+  }
+]);
